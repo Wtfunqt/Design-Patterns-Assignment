b7e2ec1a8e386579c3bfd4e94433138342a7bc9c
goadik <goadik@yandex.ru>
2016-04-22 09:18:41 -0400

    a

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,6 +1,6 @@
 public class Pizza implements Item{
 	protected String name = "Slice of Pizza";
-	protected double price = 10;
+	protected double price = 3.75;
 	
 	@Override
 	public String name() {
